f8030db1655872ea14ac853e54ef579b05030055
Ritesh <gritesh493@gmail.com>
2023-06-03 18:45:31 +0530

    Search button Working - Search Feature Added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { RestrauntCard } from "./RestaurantCard";
-import {data} from "../constant.js"
+import { data } from "../constant.js"
 import { Search } from "./Search";
 
 export const Body=()=>{
+    //Cards is updated according to state of the body
+    const [uiData,setUiData]=useState(data);
+
+    
     return(
         <div className="body">
-            <Search/>
-            {data.map((ele)=>{
+
+            <Search setUiData={setUiData}/>
+
+            
+            {uiData.map((ele)=>{
                 return(
                 <RestrauntCard details={ele} key={`${ele.data.id}`}/>
             )})}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { data } from "../constant.js"
 
-export const Search=()=>{
+export const Search=(props)=>{
     const [searchText,setSearchText]=useState("");    //Must be inside functional component.
+
+    filterdata=(text,data)=>{
+        const details = data.filter((ele)=>{return(ele.data.name.includes(text))});
+        props.setUiData(details);
+    }
+
+
 
     return(
     <div className="search-container">
         <input type="text" placeholder="Search" value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}></input>
-        <button className="btn" >Search</button>
+        <button className="btn" onClick={()=>filterdata(searchText,data)} >Search</button>  {/*Always use callback/anonymous function inside onClick */}
     </div>);
 }
