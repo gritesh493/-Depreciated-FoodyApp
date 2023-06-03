d7e2a0b7d0a5fc40d8ba22172cd5d0ffda9af8f2
Ritesh <gritesh493@gmail.com>
2023-06-03 16:02:42 +0530

    UI Update - InputBox/SearchBox added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,11 @@
 import { RestrauntCard } from "./RestaurantCard";
 import {data} from "../constant.js"
-
+import { Search } from "./Search";
 
 export const Body=()=>{
     return(
         <div className="body">
+            <Search/>
             {data.map((ele)=>{
                 return(
                 <RestrauntCard details={ele} key={`${ele.data.id}`}/>
---
+++ src/components/Search.js
@@ -0,0 +1,6 @@
+export const Search=()=>{
+    return(<div className="search-container">
+        <input type="textbox" placeholder="Search"></input>
+        <button className="btn">Search</button>
+    </div>);
+}
