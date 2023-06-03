7af81e9a66bff04f8984d4f6faa2892698da0761
Ritesh <gritesh493@gmail.com>
2023-06-03 08:58:48 +0530

    Inital react app created

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
+
+const Layout=()=>{
+    return(<> Layout </>);
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Layout/>);
