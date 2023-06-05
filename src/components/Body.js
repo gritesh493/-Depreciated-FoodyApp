2c0a769c992740ffcdf5137769f113c5be7df94b
Ritesh <gritesh493@gmail.com>
2023-06-05 08:28:17 +0530

    Restaurant Data fetched using useEffect hook - Using Async/Await inside body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RestrauntCard } from "./RestaurantCard";
 import { data } from "../constant.js"
 import { Search } from "./Search";
 
 export const Body=()=>{
-    //Cards is updated according to state of the body
-    const [uiData,setUiData]=useState(data);
+
+    const [restaurant,setRestaurant]=useState(data);
+
+    async function getRestraunts(){
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.6012849&lng=88.3312686&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        console.log(json);
+        setRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    }
+
+    useEffect(()=>{
+        getRestraunts();
+    },[]);
 
     
     return(
         <div className="body">
 
-            <Search setUiData={setUiData}/>
+            <Search setUiData={setRestaurant}/>
 
             
-            {uiData.map((ele)=>{
+            {restaurant?.map((ele)=>{
                 return(
                 <RestrauntCard details={ele} key={`${ele.data.id}`}/>
             )})}
