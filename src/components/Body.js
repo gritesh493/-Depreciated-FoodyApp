08fb6521ae93225417fb4e92a254b54ba8c9c751
Ritesh <gritesh493@gmail.com>
2023-06-05 09:22:54 +0530

    Shimmer UI component added - Used useEffect hook and Conditional Rendering

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { RestrauntCard } from "./RestaurantCard";
 import { data } from "../constant.js"
 import { Search } from "./Search";
+import {ShimmerUI} from "./ShimmerUI";
 
 export const Body=()=>{
 
-    const [restaurant,setRestaurant]=useState(data);
+    const [restaurant,setRestaurant]=useState([]);
 
     async function getRestraunts(){
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.6012849&lng=88.3312686&page_type=DESKTOP_WEB_LISTING");
@@ -19,16 +20,16 @@ export const Body=()=>{
     },[]);
 
     
-    return(
+    return (
         <div className="body">
 
             <Search setUiData={setRestaurant}/>
 
             
-            {restaurant?.map((ele)=>{
+            {(restaurant?.length==0)?(<ShimmerUI/>):(restaurant?.map((ele)=>{
                 return(
                 <RestrauntCard details={ele} key={`${ele.data.id}`}/>
-            )})}
+            )}))}
         </div>
     );
 }
---
+++ src/components/ShimmerUI.js
@@ -0,0 +1,17 @@
+export const ShimmerUI=()=>{
+    return(
+        <div id="shimmer-container">
+            <div className="scard"></div>
+            <div className="scard"></div>
+            <div className="scard"></div>
+            <div className="scard"></div>
+            <div className="scard"></div>
+            <div className="scard"></div>
+            <div className="scard"></div>
+            <div className="scard"></div>
+            <div className="scard"></div>
+        </div>
+    );
+}
+
+// export default ShimmerUI;
