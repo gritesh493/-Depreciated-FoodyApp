75af76caac676f852abc662830f47380e00e6c22
Ritesh <gritesh493@gmail.com>
2023-07-22 06:14:00 +0530

    Lifted the state of instamart section UP-Bug introduced, hide not working

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,22 +1,23 @@
 import { useState } from "react";
 
-const Section=({title,description})=>{
-    const [isVisible,setIsVisible]=useState(true);
+const Section=({title,description,isVisible,setIsVisible})=>{
     return(
         <>
         <h1 className="font-bold text-lg">{title}</h1>
-        {isVisible?<button onClick={()=>setIsVisible(false)}>Hide</button>:<button onClick={()=>setIsVisible(true)}>Show</button>}
+        {isVisible?<button onClick={()=>{setIsVisible()}}>Hide</button>:<button onClick={()=>{setIsVisible();}}>Show</button>}
         {isVisible && <p >{description}</p>}
         </>
     );
 }
 
 const Instamart=()=>{
+    const [visibleSection,setVisibleSection]=useState("about");
+
     return(
         <>
-        <Section title={"About Instamart"} key={"11"} description={"This is About Instamart"}/>
-        <Section title={"Team Instamart"} key={"22"} description={"This is Team Instamart"}/>
-        <Section title={"Careers Instamart"} key={"33"} description={"This is Career Instamart"}/>
+        <Section title={"About Instamart"} key={"11"} description={"This is About Instamart"} isVisible={visibleSection==="about"} setIsVisible={()=>setVisibleSection("about")}/>
+        <Section title={"Team Instamart"} key={"22"} description={"This is Team Instamart"} isVisible={visibleSection==="team"} setIsVisible={()=>setVisibleSection("team")}/>
+        <Section title={"Careers Instamart"} key={"33"} description={"This is Career Instamart"} isVisible={visibleSection==="career"} setIsVisible={()=>setVisibleSection("career")}/>
         </>
     );
 }
